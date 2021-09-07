c99527c11a35b786b07e16eb42ac234c96b3eb1b
Cameron Rojas <rscameron7@gmail.com>
2021-09-06 18:24:23 -0700

    built out layout, header, navbar components

--- ECOM/src/components/layout.js
+++ ECOM/src/components/layout.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import Header from './headerNav/header';
-import NavBar from './headerNav/navbar';
 
 class Layout extends Component {
   render() {
     return (
       <div>
-        <h1>ECOM</h1>
+        <h1>Ecom</h1>
         {...this.props.children}
-        <Header />
-        <NavBar />
       </div>
     );
   }
